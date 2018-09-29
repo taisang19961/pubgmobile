e2a296f40a1648684239c3a73ed12b7438cd9cc7
Nguyen Tan Tai <taisang19961@gmail.com>
2018-09-29 23:44:53 +0700

    n

---
+++ public.lua
@@ -0,0 +1,2 @@
+local url = 'https://raw.githubusercontent.com/taisang19961/pubgmobile/master/source.lua'
+pcall(load(gg.makeRequest(url).content))
--- source.lua
+++
@@ -1,4 +0,0 @@
-local thongbao = "⚠ CẢNH BÁO ⚠\nGame đang càn quét hack trên android.\nAnh em cẩn thận không nên sử dụng hack trong thời gian này."
-ask = gg.alert(thongbao, "Đã hiểu")
-
-if ask == 1 then os.exit() end
