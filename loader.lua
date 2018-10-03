acc2c2479ba1e4cefd54fc19e14ecebe52e63327
Nguyen Tan Tai <taisang19961@gmail.com>
2018-10-03 14:07:30 +0700

    m

--- loader.lua
+++ loader.lua
@@ -1,2 +1,2 @@
-local url = 'https://raw.githubusercontent.com/taisang19961/pubgmobile/master/script.ver.0.8.1.9350.lua'
+local url = 'https://rawgit.com/taisang19961/pubgmobile/master/script.ver.0.8.1.9350.lua'
 pcall(load(gg.makeRequest(url).content))
